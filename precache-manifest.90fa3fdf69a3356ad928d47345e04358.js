62fdd1d01c591b0a1460257c76ff1f0a150a0ba5
ufufuru <obadere@gmail.com>
2019-03-27 22:11:49 -0500

    Updates

---
+++ precache-manifest.90fa3fdf69a3356ad928d47345e04358.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2536cf0dbb3051391bfe2a564461178b",
+    "url": "/static/media/vcs.2536cf0d.png"
+  },
+  {
+    "revision": "de5b86466a4a1c36b57e52c0e74bad08",
+    "url": "/static/media/mk.de5b8646.png"
+  },
+  {
+    "revision": "2c38bc8c00c4d13d7b7ae2f5d85ecd8d",
+    "url": "/static/media/fuji.2c38bc8c.jpg"
+  },
+  {
+    "revision": "c0d1e12a368355deed139a5262bc1970",
+    "url": "/static/media/ddd.c0d1e12a.png"
+  },
+  {
+    "revision": "bc33d07150f51e2e17d8b77d286c9f52",
+    "url": "/static/media/db.bc33d071.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b7e083b87bef8aace33",
+    "url": "/static/js/main.67388f64.chunk.js"
+  },
+  {
+    "revision": "4688cfd0b24b23207cb7",
+    "url": "/static/js/2.e0f7f09c.chunk.js"
+  },
+  {
+    "revision": "4b7e083b87bef8aace33",
+    "url": "/static/css/main.03748707.chunk.css"
+  },
+  {
+    "revision": "a81ac1466730682f7a212acd2c35f580",
+    "url": "/index.html"
+  }
+];
